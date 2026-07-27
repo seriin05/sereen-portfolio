d5343e5569f0b7f3889feefbfab5703a1a0387b1
seriin05 <asereen53@gmail.com>
2026-07-27 21:59:12 +0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,8 +13,8 @@ return false ;
 alert("form submitted successfull");
 return true;}
 
-const menuBtn = document.querySelector(".fa-bars");
-const navMenu = document.querySelector(".nav-menu");
+var menuBtn = document.getElementById("menu-btn");
+var navMenu = document.querySelector(".nav-menu");
 
 menuBtn.addEventListener("click", function () {
     navMenu.classList.toggle("active");
